401a8c9cfda53cb713948b74b2a0759494403477
eyalhub <95219363+eyalhub@users.noreply.github.com>
2022-01-19 17:21:57 +0200

    added SocketServer class

---
+++ BL/SocketServer.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class SocketServer
+    {
+        private readonly Socket _server;
+
+        public SocketServer(Socket socket, IPEndPoint ipEndPoint)
+        {
+            _server = socket;
+            _server.Bind(ipEndPoint);
+        }
+
+        public void Start()
+        {
+            _server.Listen();
+            while (true)
+            {
+                Socket client = _server.Accept();
+                Task t = new Task(() => HandleClinet(client));
+                t.Start();
+            }
+        }
+
+        private void HandleClinet(Socket client)
+        {
+
+        }
+    }
+}
