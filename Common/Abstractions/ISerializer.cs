4fe021eeab938af6df8bf6a2200076bf42ff1c38
stav10 <95219363+eyalhub@users.noreply.github.com>
2022-01-20 10:16:20 +0200

    added Serializer

---
+++ Common/Abstractions/ISerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Abstractions
+{
+    public interface ISerializer<T, K>
+    {
+        public T Serialize(K value);
+        public K Desrialize(T value);
+    }
+}
---
+++ Common/Serializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Common.Abstractions;
+
+namespace Common
+{
+    class Serializer : ISerializer<object, byte[]>
+    {
+        public byte[] Desrialize(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Serialize(byte[] value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
